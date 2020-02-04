617bdaa063bb6dd688dd4044622c4dce556c5902
robloxguyed@gmail.com <>
2020-02-04 15:47:18 -0500

    This is my first commit

---
+++ src/firstgithub/FirstGithub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgithub;
+
+/**
+ *
+ * @author roblo
+ */
+public class FirstGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    static void Bednar(){
+        System.out.println("Jeremy Bednar!");
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bednar();
+    }
+    
+}
